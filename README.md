21ff65f894443724994c61ffebce37a55112a60f
ezekg <ezekg@yahoo.com>
2018-02-27 17:30:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# New Tab Vocab
+
+Google Chrome extension that shows you a new definition every time you open a new tab.
+
+![image](https://user-images.githubusercontent.com/6979737/36795477-c0e5ac70-1c68-11e8-9b73-f1cfd576549a.png)
+
+## Installation
+
+1. Clone this repo to any folder
+1. Navigate to chrome://extensions/ in Google Chrome
+1. Check "Developer Mode" in the upper right
+1. Click "Unload Packed Extension..." and select the cloned repo folder
+1. Done! :tada: :balloon: :confetti_ball: :tada: :balloon: :confetti_ball:
---
+++ main.js
@@ -0,0 +1,42 @@
+const GRE_VOCABULARY_ENDPOINT = `https://gre.economist.com/gre-vocabulary.json`
+
+async function main() {
+  let words
+  if (localStorage.GRE_VOCABULARY) {
+    words = JSON.parse(localStorage.GRE_VOCABULARY)
+  } else {
+    const res = await fetch(GRE_VOCABULARY_ENDPOINT, { headers: { accept: 'application/json' } })
+    words = await res.json()
+
+    // Cache for later
+    localStorage.GRE_VOCABULARY = JSON.stringify(words)
+  }
+
+  let {
+    word,
+    pronunciation,
+    definition,
+    passage,
+    partOfSpeech,
+  } = words[Math.floor(Math.random() * words.length)]
+
+  // Some words contain newlines for some reason
+  word = word.trim()
+
+  document.body.innerHTML = `
+    <div class='vocab'>
+      <h1 class='vocab__word'>
+        ${word}
+      </h1>
+      <hr class='vocab__hr'>
+      <blockquote class='vocab__passage'>
+        &ldquo;${passage.replace(new RegExp(`(${word}[a-z]*)`, 'gi'), '<em>$1</em>')}&rdquo;
+      </blockquote>
+      <p class='vocab__definition'>
+        ${pronunciation} [${partOfSpeech}] &ndash; ${definition}
+      </p>
+    </div>
+  `
+}
+
+main()
